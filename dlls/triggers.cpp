0317915947a78109798794b8364e5f445c3d68cd
Sam <duo929@hotmail.com>
2019-01-18 22:19:54 +0100

    Implement trigger_kill_nogib

--- dlls/triggers.cpp
+++ dlls/triggers.cpp
@@ -2459,3 +2459,33 @@ void CTriggerPlayerFreeze::Use( CBaseEntity* pActivator, CBaseEntity* pCaller, U
 
 	pPlayer->EnableControl( m_bUnFrozen );
 }
+
+/**
+*	@brief Kills anything that touches it without gibbing it
+*/
+class CTriggerKillNoGib : public CBaseTrigger
+{
+public:
+	void Spawn() override;
+
+	void KillTouch( CBaseEntity* pOther );
+};
+
+LINK_ENTITY_TO_CLASS( trigger_kill_nogib, CTriggerKillNoGib );
+
+void CTriggerKillNoGib::Spawn()
+{
+	InitTrigger();
+
+	SetTouch( &CTriggerKillNoGib::KillTouch );
+	SetUse( nullptr );
+
+	//TODO: this needs to be removed in order to function
+	pev->solid = SOLID_NOT;
+}
+
+void CTriggerKillNoGib::KillTouch( CBaseEntity* pOther )
+{
+	if( pOther->pev->takedamage != DAMAGE_NO )
+		pOther->TakeDamage( pev, pOther->pev, 500000, DMG_NEVERGIB );
+}
