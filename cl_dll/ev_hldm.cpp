67f75f9ebbfb44e781d9b0eab5419025142331be
Sam <duo929@hotmail.com>
2019-01-18 23:11:18 +0100

    Add new bullet types to client switches

--- cl_dll/ev_hldm.cpp
+++ cl_dll/ev_hldm.cpp
@@ -292,6 +292,9 @@ void EV_HLDM_DecalGunshot( pmtrace_t *pTrace, int iBulletType )
 		case BULLET_MONSTER_MP5:
 		case BULLET_PLAYER_BUCKSHOT:
 		case BULLET_PLAYER_357:
+		case BULLET_PLAYER_556:
+		case BULLET_PLAYER_762:
+		case BULLET_PLAYER_EAGLE:
 		default:
 			// smoke and decal
 			EV_HLDM_GunshotDecalTrace( pTrace, EV_HLDM_DamageDecal( pe ) );
@@ -334,6 +337,9 @@ int EV_HLDM_CheckTracer( int idx, float *vecSrc, float *end, float *forward, flo
 		case BULLET_MONSTER_MP5:
 		case BULLET_MONSTER_9MM:
 		case BULLET_MONSTER_12MM:
+		case BULLET_PLAYER_556:
+		case BULLET_PLAYER_762:
+		case BULLET_PLAYER_EAGLE:
 		default:
 			EV_CreateTracer( vecTracerSrc, end );
 			break;
@@ -433,6 +439,20 @@ void EV_HLDM_FireBullets( int idx, float *forward, float *right, float *up, int 
 				
 				break;
 
+			case BULLET_PLAYER_EAGLE:
+				EV_HLDM_PlayTextureSound( idx, &tr, vecSrc, vecEnd, iBulletType );
+				EV_HLDM_DecalGunshot( &tr, iBulletType );
+				break;
+
+			case BULLET_PLAYER_762:
+				EV_HLDM_PlayTextureSound( idx, &tr, vecSrc, vecEnd, iBulletType );
+				EV_HLDM_DecalGunshot( &tr, iBulletType );
+				break;
+
+			case BULLET_PLAYER_556:
+				EV_HLDM_PlayTextureSound( idx, &tr, vecSrc, vecEnd, iBulletType );
+				EV_HLDM_DecalGunshot( &tr, iBulletType );
+				break;
 			}
 		}
 
