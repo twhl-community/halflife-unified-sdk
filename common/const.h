f6032cc4db0b492c4ca73ed43bf0f3767baff5b4
Sam <duo929@hotmail.com>
2019-01-18 23:11:25 +0100

    Add WaterLevel enum

--- common/const.h
+++ common/const.h
@@ -779,5 +779,32 @@ typedef struct
 	int		hitgroup;		// 0 == generic, non zero is specific body part
 } trace_t;
 
+/**
+*	@brief Constants for entvars_t::waterlevel
+*	@see entvars_t::waterlevel
+*/
+enum WaterLevel
+{
+	/**
+	*	Not in water at all
+	*/
+	WATERLEVEL_DRY = 0,
+
+	/**
+	*	Standing in water, feet only
+	*/
+	WATERLEVEL_FEET = 1,
+
+	/**
+	*	Halfway submerged
+	*/
+	WATERLEVEL_WAIST = 2,
+
+	/**
+	*	Submerged up to eyes or more
+	*/
+	WATERLEVEL_HEAD = 3
+};
+
 #endif
 
