843f31da2d3520200d2152f02d4be2d0fc0da61d
Sam <duo929@hotmail.com>
2019-01-19 12:51:34 +0100

    Increase HUD weapon list slots to 7

--- dlls/cdll_dll.h
+++ dlls/cdll_dll.h
@@ -23,7 +23,7 @@
 #define MAX_WEAPONS		32		// ???
 const int SV_MAX_PLAYERS = 32;
 
-#define MAX_WEAPON_SLOTS		5	// hud item selection slots
+#define MAX_WEAPON_SLOTS		7	// hud item selection slots
 #define MAX_ITEM_TYPES			6	// hud item selection slots
 
 #define MAX_ITEMS				5	// hard coded item types
