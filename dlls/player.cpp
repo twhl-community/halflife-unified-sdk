8ffc7a8babaa2ff29b1bee7995e54ab5effad2b8
Solokiller <duo929@hotmail.com>
2021-02-01 22:31:47 +0100

    Reset jpj physics keyvalue to 0 on player spawn

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -3203,6 +3203,7 @@ void CBasePlayer::Spawn( void )
 
 	g_engfuncs.pfnSetPhysicsKeyValue( edict(), "slj", "0" );
 	g_engfuncs.pfnSetPhysicsKeyValue( edict(), "hl", "1" );
+	g_engfuncs.pfnSetPhysicsKeyValue(edict(), "jpj", "0");
 
 	pev->fov = m_iFOV				= 0;// init field of view.
 	m_iClientFOV		= -1; // make sure fov reset is sent
