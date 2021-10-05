4be610757acbfe8bc740ff497d86586804e9169c
Sam V <sam.vanheer@outlook.com>
2021-10-05 12:36:56 +0200

    Add trigger_kill_nogib Resolves #86

--- src/game/server/entities/triggers.cpp
+++ src/game/server/entities/triggers.cpp
@@ -2516,6 +2516,36 @@ void CTriggerPlayerFreeze::PlayerFreezeDelay()
 	SetThink(nullptr);
 }
 
+/**
+*	@brief Kills anything that touches it without gibbing it
+*/
+class CTriggerKillNoGib : public CBaseTrigger
+{
+public:
+	void Spawn() override;
+
+	void KillTouch(CBaseEntity* pOther);
+};
+
+LINK_ENTITY_TO_CLASS(trigger_kill_nogib, CTriggerKillNoGib);
+
+void CTriggerKillNoGib::Spawn()
+{
+	InitTrigger();
+
+	SetTouch(&CTriggerKillNoGib::KillTouch);
+	SetUse(nullptr);
+
+	//TODO: this needs to be removed in order to function
+	pev->solid = SOLID_NOT;
+}
+
+void CTriggerKillNoGib::KillTouch(CBaseEntity* pOther)
+{
+	if (pOther->pev->takedamage != DAMAGE_NO)
+		pOther->TakeDamage(pev, pOther->pev, 500000, DMG_NEVERGIB);
+}
+
 class CTriggerXenReturn : public CBaseTrigger
 {
 public:
