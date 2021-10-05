46b77631023534a6f7ec8c5a3482f81ae3c9ee24
Sam V <sam.vanheer@outlook.com>
2021-10-05 17:01:57 +0200

    Add monster_houndeye_dead Resolves #36

--- src/game/server/entities/NPCs/aliens/houndeye.cpp
+++ src/game/server/entities/NPCs/aliens/houndeye.cpp
@@ -1302,3 +1302,59 @@ Schedule_t* CHoundeye::GetSchedule()
 
 	return CSquadMonster::GetSchedule();
 }
+
+//=========================================================
+// DEAD HOUNDEYE PROP
+//=========================================================
+class CDeadHoundeye : public CBaseMonster
+{
+public:
+	void Spawn() override;
+	int	Classify() override { return	CLASS_ALIEN_PASSIVE; }
+
+	void KeyValue(KeyValueData* pkvd) override;
+
+	int	m_iPose;// which sequence to display	-- temporary, don't need to save
+	static char* m_szPoses[3];
+};
+
+char* CDeadHoundeye::m_szPoses[] = {"dead"};
+
+void CDeadHoundeye::KeyValue(KeyValueData* pkvd)
+{
+	if (FStrEq(pkvd->szKeyName, "pose"))
+	{
+		m_iPose = atoi(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else
+		CBaseMonster::KeyValue(pkvd);
+}
+
+LINK_ENTITY_TO_CLASS(monster_houndeye_dead, CDeadHoundeye);
+
+//=========================================================
+// ********** DeadHoundeye SPAWN **********
+//=========================================================
+void CDeadHoundeye::Spawn()
+{
+	PRECACHE_MODEL("models/houndeye_dead.mdl");
+	SET_MODEL(ENT(pev), "models/houndeye_dead.mdl");
+
+	pev->effects = 0;
+	pev->yaw_speed = 8;
+	pev->sequence = 0;
+	m_bloodColor = BLOOD_COLOR_GREEN;
+
+	pev->sequence = LookupSequence(m_szPoses[m_iPose]);
+
+	if (pev->sequence == -1)
+	{
+		ALERT(at_console, "Dead houndeye with bad pose\n");
+	}
+
+	// Corpses have less health
+	pev->health = 8;
+
+	MonsterInitDead();
+}
