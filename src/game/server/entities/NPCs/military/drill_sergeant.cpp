c77e9e6339ae4ba7d171ce8fb43047836da83b19
Sam V <sam.vanheer@outlook.com>
2021-10-05 16:17:23 +0200

    Add monster_drillsergeant Resolves #29

---
+++ src/game/server/entities/NPCs/military/drill_sergeant.cpp
@@ -0,0 +1,684 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+
+#include	"extdll.h"
+#include	"util.h"
+#include	"cbase.h"
+#include	"monsters.h"
+#include	"talkmonster.h"
+#include	"schedule.h"
+#include	"defaultai.h"
+#include	"scripted.h"
+#include	"weapons.h"
+#include	"soundent.h"
+
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+// first flag is barney dying for scripted sequences?
+#define		BARNEY_AE_DRAW		( 2 )
+#define		BARNEY_AE_SHOOT		( 3 )
+#define		BARNEY_AE_HOLSTER	( 4 )
+
+#define	BARNEY_BODY_GUNHOLSTERED	0
+#define	BARNEY_BODY_GUNDRAWN		1
+#define BARNEY_BODY_GUNGONE			2
+
+/**
+*	@brief A copy of Barney that speaks military loudly
+*/
+class CDrillSergeant : public CTalkMonster
+{
+public:
+	int		Save(CSave& save) override;
+	int		Restore(CRestore& restore) override;
+	static	TYPEDESCRIPTION m_SaveData[];
+
+	CUSTOM_SCHEDULES;
+
+	int ISoundMask() override;
+
+	int Classify() override;
+
+	void SetYawSpeed() override;
+
+	void DeclineFollowing() override;
+
+	MONSTERSTATE GetIdealState() override;
+
+	Schedule_t* GetSchedule() override;
+
+	void Killed(entvars_t* pevAttacker, int iGib) override;
+
+	void TraceAttack(entvars_t* pevAttacker, float flDamage, Vector vecDir, TraceResult* ptr, int bitsDamageType) override;
+
+	void DeathSound() override;
+
+	void Spawn() override;
+
+	void StartTask(Task_t* pTask) override;
+
+	void AlertSound() override;
+
+	Schedule_t* GetScheduleOfType(int Type) override;
+
+	void RunTask(Task_t* pTask) override;
+
+	void PainSound() override;
+
+	BOOL CheckRangeAttack1(float flDot, float flDist) override;
+
+	int TakeDamage(entvars_t* pevInflictor, entvars_t* pevAttacker, float flDamage, int bitsDamageType) override;
+
+	void Precache() override;
+
+	void BarneyFirePistol();
+
+	void HandleAnimEvent(MonsterEvent_t* pEvent) override;
+
+	void TalkInit();
+
+	int ObjectCaps() override;
+
+public:
+	BOOL m_fGunDrawn;
+	float m_painTime;
+	float m_checkAttackTime;
+	BOOL m_lastAttackCheck;
+	float m_flPlayerDamage;
+};
+
+LINK_ENTITY_TO_CLASS(monster_drillsergeant, CDrillSergeant);
+
+TYPEDESCRIPTION	CDrillSergeant::m_SaveData[] =
+{
+	DEFINE_FIELD(CDrillSergeant, m_fGunDrawn, FIELD_BOOLEAN),
+	DEFINE_FIELD(CDrillSergeant, m_painTime, FIELD_TIME),
+	DEFINE_FIELD(CDrillSergeant, m_checkAttackTime, FIELD_TIME),
+	DEFINE_FIELD(CDrillSergeant, m_lastAttackCheck, FIELD_BOOLEAN),
+	DEFINE_FIELD(CDrillSergeant, m_flPlayerDamage, FIELD_FLOAT),
+};
+
+IMPLEMENT_SAVERESTORE(CDrillSergeant, CTalkMonster);
+
+//=========================================================
+// AI Schedules Specific to this monster
+//=========================================================
+Task_t	tlDrFollow[] =
+{
+	{ TASK_MOVE_TO_TARGET_RANGE,(float)128		},	// Move within 128 of target ent (client)
+	{ TASK_SET_SCHEDULE,		(float)SCHED_TARGET_FACE },
+};
+
+Schedule_t	slDrFollow[] =
+{
+	{
+		tlDrFollow,
+		ARRAYSIZE(tlDrFollow),
+		bits_COND_NEW_ENEMY |
+		bits_COND_LIGHT_DAMAGE |
+		bits_COND_HEAVY_DAMAGE |
+		bits_COND_HEAR_SOUND |
+		bits_COND_PROVOKED,
+		bits_SOUND_DANGER,
+		"Follow"
+	},
+};
+
+//=========================================================
+// BarneyDraw- much better looking draw schedule for when
+// barney knows who he's gonna attack.
+//=========================================================
+Task_t	tlDrrneyEnemyDraw[] =
+{
+	{ TASK_STOP_MOVING,					0				},
+	{ TASK_FACE_ENEMY,					0				},
+	{ TASK_PLAY_SEQUENCE_FACE_ENEMY,	(float)ACT_ARM },
+};
+
+Schedule_t slDrrneyEnemyDraw[] =
+{
+	{
+		tlDrrneyEnemyDraw,
+		ARRAYSIZE(tlDrrneyEnemyDraw),
+		0,
+		0,
+		"Barney Enemy Draw"
+	}
+};
+
+Task_t	tlDrFaceTarget[] =
+{
+	{ TASK_SET_ACTIVITY,		(float)ACT_IDLE },
+	{ TASK_FACE_TARGET,			(float)0		},
+	{ TASK_SET_ACTIVITY,		(float)ACT_IDLE },
+	{ TASK_SET_SCHEDULE,		(float)SCHED_TARGET_CHASE },
+};
+
+Schedule_t	slDrFaceTarget[] =
+{
+	{
+		tlDrFaceTarget,
+		ARRAYSIZE(tlDrFaceTarget),
+		bits_COND_CLIENT_PUSH |
+		bits_COND_NEW_ENEMY |
+		bits_COND_LIGHT_DAMAGE |
+		bits_COND_HEAVY_DAMAGE |
+		bits_COND_HEAR_SOUND |
+		bits_COND_PROVOKED,
+		bits_SOUND_DANGER,
+		"FaceTarget"
+	},
+};
+
+
+Task_t	tlIdleDrStand[] =
+{
+	{ TASK_STOP_MOVING,			0				},
+	{ TASK_SET_ACTIVITY,		(float)ACT_IDLE },
+	{ TASK_WAIT,				(float)2		}, // repick IDLESTAND every two seconds.
+	{ TASK_TLK_HEADRESET,		(float)0		}, // reset head position
+};
+
+Schedule_t	slIdleDrStand[] =
+{
+	{
+		tlIdleDrStand,
+		ARRAYSIZE(tlIdleDrStand),
+		bits_COND_NEW_ENEMY |
+		bits_COND_LIGHT_DAMAGE |
+		bits_COND_HEAVY_DAMAGE |
+		bits_COND_HEAR_SOUND |
+		bits_COND_SMELL |
+		bits_COND_PROVOKED,
+
+		bits_SOUND_COMBAT |// sound flags - change these, and you'll break the talking code.
+		//bits_SOUND_PLAYER		|
+		//bits_SOUND_WORLD		|
+
+		bits_SOUND_DANGER |
+		bits_SOUND_MEAT |// scents
+		bits_SOUND_CARCASS |
+		bits_SOUND_GARBAGE,
+		"IdleStand"
+	},
+};
+
+DEFINE_CUSTOM_SCHEDULES(CDrillSergeant)
+{
+	slDrFollow,
+		slDrrneyEnemyDraw,
+		slDrFaceTarget,
+		slIdleDrStand,
+};
+
+
+IMPLEMENT_CUSTOM_SCHEDULES(CDrillSergeant, CTalkMonster);
+
+int CDrillSergeant::ISoundMask()
+{
+	return bits_SOUND_WORLD |
+		bits_SOUND_COMBAT |
+		bits_SOUND_CARCASS |
+		bits_SOUND_MEAT |
+		bits_SOUND_GARBAGE |
+		bits_SOUND_DANGER |
+		bits_SOUND_PLAYER;
+}
+
+int CDrillSergeant::Classify()
+{
+	return CLASS_HUMAN_MILITARY_FRIENDLY;
+}
+
+void CDrillSergeant::SetYawSpeed()
+{
+	auto speed = 90;
+
+	if (m_Activity != ACT_RUN)
+		speed = 70;
+
+	pev->yaw_speed = speed;
+}
+
+void CDrillSergeant::DeclineFollowing()
+{
+	PlaySentence("DR_POK", 2, VOL_NORM, ATTN_NORM);
+}
+
+MONSTERSTATE CDrillSergeant::GetIdealState()
+{
+	return CBaseMonster::GetIdealState();
+}
+
+Schedule_t* CDrillSergeant::GetSchedule()
+{
+	if (HasConditions(bits_COND_HEAR_SOUND))
+	{
+		CSound* pSound;
+		pSound = PBestSound();
+
+		ASSERT(pSound != NULL);
+		if (pSound && (pSound->m_iType & bits_SOUND_DANGER))
+			return GetScheduleOfType(SCHED_TAKE_COVER_FROM_BEST_SOUND);
+	}
+	if (HasConditions(bits_COND_ENEMY_DEAD) && FOkToSpeak())
+	{
+		PlaySentence("DR_KILL", 4, VOL_NORM, ATTN_NORM);
+	}
+
+	switch (m_MonsterState)
+	{
+	case MONSTERSTATE_COMBAT:
+	{
+		// dead enemy
+		if (HasConditions(bits_COND_ENEMY_DEAD))
+		{
+			// call base class, all code to handle dead enemies is centralized there.
+			return CBaseMonster::GetSchedule();
+		}
+
+		// always act surprized with a new enemy
+		if (HasConditions(bits_COND_NEW_ENEMY) && HasConditions(bits_COND_LIGHT_DAMAGE))
+			return GetScheduleOfType(SCHED_SMALL_FLINCH);
+
+		// wait for one schedule to draw gun
+		if (!m_fGunDrawn)
+			return GetScheduleOfType(SCHED_ARM_WEAPON);
+
+		if (HasConditions(bits_COND_HEAVY_DAMAGE))
+			return GetScheduleOfType(SCHED_TAKE_COVER_FROM_ENEMY);
+	}
+	break;
+
+	case MONSTERSTATE_ALERT:
+	case MONSTERSTATE_IDLE:
+		if (HasConditions(bits_COND_LIGHT_DAMAGE | bits_COND_HEAVY_DAMAGE))
+		{
+			// flinch if hurt
+			return GetScheduleOfType(SCHED_SMALL_FLINCH);
+		}
+
+		if (m_hEnemy == NULL && IsFollowing())
+		{
+			if (!m_hTargetEnt->IsAlive())
+			{
+				// UNDONE: Comment about the recently dead player here?
+				StopFollowing(FALSE);
+				break;
+			}
+			else
+			{
+				if (HasConditions(bits_COND_CLIENT_PUSH))
+				{
+					return GetScheduleOfType(SCHED_MOVE_AWAY_FOLLOW);
+				}
+				return GetScheduleOfType(SCHED_TARGET_FACE);
+			}
+		}
+
+		if (HasConditions(bits_COND_CLIENT_PUSH))
+		{
+			return GetScheduleOfType(SCHED_MOVE_AWAY);
+		}
+
+		// try to say something about smells
+		TrySmellTalk();
+		break;
+	}
+
+	return CTalkMonster::GetSchedule();
+}
+
+void CDrillSergeant::Killed(entvars_t* pevAttacker, int iGib)
+{
+	SetUse(NULL);
+	CTalkMonster::Killed(pevAttacker, iGib);
+}
+
+void CDrillSergeant::TraceAttack(entvars_t* pevAttacker, float flDamage, Vector vecDir, TraceResult* ptr, int bitsDamageType)
+{
+	switch (ptr->iHitgroup)
+	{
+	case HITGROUP_CHEST:
+	case HITGROUP_STOMACH:
+		if (bitsDamageType & (DMG_BULLET | DMG_SLASH | DMG_BLAST))
+		{
+			flDamage = flDamage / 2;
+		}
+		break;
+	case 10:
+		if (bitsDamageType & (DMG_BULLET | DMG_SLASH | DMG_CLUB))
+		{
+			flDamage -= 20;
+			if (flDamage <= 0)
+			{
+				UTIL_Ricochet(ptr->vecEndPos, 1.0);
+				flDamage = 0.01;
+			}
+		}
+		// always a head shot
+		ptr->iHitgroup = HITGROUP_HEAD;
+		break;
+	}
+
+	CTalkMonster::TraceAttack(pevAttacker, flDamage, vecDir, ptr, bitsDamageType);
+}
+
+void CDrillSergeant::DeathSound()
+{
+	switch (RANDOM_LONG(0, 2))
+	{
+	case 0: EMIT_SOUND_DYN(ENT(pev), CHAN_VOICE, "barney/ba_die1.wav", 1, ATTN_NORM, 0, GetVoicePitch()); break;
+	case 1: EMIT_SOUND_DYN(ENT(pev), CHAN_VOICE, "barney/ba_die2.wav", 1, ATTN_NORM, 0, GetVoicePitch()); break;
+	case 2: EMIT_SOUND_DYN(ENT(pev), CHAN_VOICE, "barney/ba_die3.wav", 1, ATTN_NORM, 0, GetVoicePitch()); break;
+	}
+}
+
+void CDrillSergeant::Spawn()
+{
+	Precache();
+
+	SET_MODEL(ENT(pev), "models/drill.mdl");
+	UTIL_SetSize(pev, VEC_HUMAN_HULL_MIN, VEC_HUMAN_HULL_MAX);
+
+	pev->solid = SOLID_SLIDEBOX;
+	pev->movetype = MOVETYPE_STEP;
+	m_bloodColor = BLOOD_COLOR_RED;
+	pev->health = gSkillData.barneyHealth;
+	pev->view_ofs = Vector(0, 0, 50);// position of the eyes relative to monster's origin.
+	m_flFieldOfView = VIEW_FIELD_WIDE; // NOTE: we need a wide field of view so npc will notice player and say hello
+	m_MonsterState = MONSTERSTATE_NONE;
+
+	pev->body = 0; // gun in holster
+	m_fGunDrawn = FALSE;
+
+	m_afCapability = bits_CAP_HEAR | bits_CAP_TURN_HEAD | bits_CAP_DOORS_GROUP;
+
+	MonsterInit();
+	SetUse(&CTalkMonster::FollowerUse);
+}
+
+void CDrillSergeant::StartTask(Task_t* pTask)
+{
+	CTalkMonster::StartTask(pTask);
+}
+
+void CDrillSergeant::AlertSound()
+{
+	if (m_hEnemy != NULL)
+	{
+		if (FOkToSpeak())
+		{
+			PlaySentence("DR_ATTACK", RANDOM_FLOAT(2.8, 3.2), VOL_NORM, ATTN_IDLE);
+		}
+	}
+}
+
+Schedule_t* CDrillSergeant::GetScheduleOfType(int Type)
+{
+	Schedule_t* psched;
+
+	switch (Type)
+	{
+	case SCHED_ARM_WEAPON:
+		if (m_hEnemy != NULL)
+		{
+			// face enemy, then draw.
+			return slDrrneyEnemyDraw;
+		}
+		break;
+
+		// Hook these to make a looping schedule
+	case SCHED_TARGET_FACE:
+		// call base class default so that barney will talk
+		// when 'used' 
+		psched = CTalkMonster::GetScheduleOfType(Type);
+
+		if (psched == slIdleStand)
+			return slDrFaceTarget;	// override this for different target face behavior
+		else
+			return psched;
+
+	case SCHED_TARGET_CHASE:
+		return slDrFollow;
+
+	case SCHED_IDLE_STAND:
+		// call base class default so that scientist will talk
+		// when standing during idle
+		psched = CTalkMonster::GetScheduleOfType(Type);
+
+		if (psched == slIdleStand)
+		{
+			// just look straight ahead.
+			return slIdleDrStand;
+		}
+		else
+			return psched;
+	}
+
+	return CTalkMonster::GetScheduleOfType(Type);
+}
+
+void CDrillSergeant::RunTask(Task_t* pTask)
+{
+	switch (pTask->iTask)
+	{
+	case TASK_RANGE_ATTACK1:
+		if (m_hEnemy != NULL && (m_hEnemy->IsPlayer()))
+		{
+			pev->framerate = 1.5;
+		}
+		CTalkMonster::RunTask(pTask);
+		break;
+	default:
+		CTalkMonster::RunTask(pTask);
+		break;
+	}
+}
+
+void CDrillSergeant::PainSound()
+{
+	if (gpGlobals->time < m_painTime)
+		return;
+
+	m_painTime = gpGlobals->time + RANDOM_FLOAT(0.5, 0.75);
+
+	switch (RANDOM_LONG(0, 2))
+	{
+	case 0: EMIT_SOUND_DYN(ENT(pev), CHAN_VOICE, "barney/ba_pain1.wav", 1, ATTN_NORM, 0, GetVoicePitch()); break;
+	case 1: EMIT_SOUND_DYN(ENT(pev), CHAN_VOICE, "barney/ba_pain2.wav", 1, ATTN_NORM, 0, GetVoicePitch()); break;
+	case 2: EMIT_SOUND_DYN(ENT(pev), CHAN_VOICE, "barney/ba_pain3.wav", 1, ATTN_NORM, 0, GetVoicePitch()); break;
+	}
+}
+
+BOOL CDrillSergeant::CheckRangeAttack1(float flDot, float flDist)
+{
+	if (flDist <= 1024 && flDot >= 0.5)
+	{
+		if (gpGlobals->time > m_checkAttackTime)
+		{
+			TraceResult tr;
+
+			Vector shootOrigin = pev->origin + Vector(0, 0, 55);
+			CBaseEntity* pEnemy = m_hEnemy;
+			Vector shootTarget = ((pEnemy->BodyTarget(shootOrigin) - pEnemy->pev->origin) + m_vecEnemyLKP);
+			UTIL_TraceLine(shootOrigin, shootTarget, dont_ignore_monsters, ENT(pev), &tr);
+			m_checkAttackTime = gpGlobals->time + 1;
+			if (tr.flFraction == 1.0 || (tr.pHit != NULL && CBaseEntity::Instance(tr.pHit) == pEnemy))
+				m_lastAttackCheck = TRUE;
+			else
+				m_lastAttackCheck = FALSE;
+			m_checkAttackTime = gpGlobals->time + 1.5;
+		}
+		return m_lastAttackCheck;
+	}
+	return FALSE;
+}
+
+int CDrillSergeant::TakeDamage(entvars_t* pevInflictor, entvars_t* pevAttacker, float flDamage, int bitsDamageType)
+{
+	// make sure friends talk about it if player hurts talkmonsters...
+	int ret = CTalkMonster::TakeDamage(pevInflictor, pevAttacker, flDamage, bitsDamageType);
+	if (!IsAlive() || pev->deadflag == DEAD_DYING)
+		return ret;
+
+	if (m_MonsterState != MONSTERSTATE_PRONE && (pevAttacker->flags & FL_CLIENT))
+	{
+		m_flPlayerDamage += flDamage;
+
+		// This is a heurstic to determine if the player intended to harm me
+		// If I have an enemy, we can't establish intent (may just be crossfire)
+		if (m_hEnemy == NULL)
+		{
+			// If the player was facing directly at me, or I'm already suspicious, get mad
+			if ((m_afMemory & bits_MEMORY_SUSPICIOUS) || IsFacing(pevAttacker, pev->origin))
+			{
+				// Alright, now I'm pissed!
+				PlaySentence("DR_MAD", 4, VOL_NORM, ATTN_NORM);
+
+				Remember(bits_MEMORY_PROVOKED);
+				StopFollowing(TRUE);
+			}
+			else
+			{
+				// Hey, be careful with that
+				PlaySentence("DR_SHOT", 4, VOL_NORM, ATTN_NORM);
+				Remember(bits_MEMORY_SUSPICIOUS);
+			}
+		}
+		else if (!(m_hEnemy->IsPlayer()) && pev->deadflag == DEAD_NO)
+		{
+			PlaySentence("DR_SHOT", 4, VOL_NORM, ATTN_NORM);
+		}
+	}
+
+	return ret;
+}
+
+void CDrillSergeant::Precache()
+{
+	PRECACHE_MODEL("models/drill.mdl");
+
+	PRECACHE_SOUND("barney/ba_attack1.wav");
+	PRECACHE_SOUND("barney/ba_attack2.wav");
+
+	PRECACHE_SOUND("barney/ba_pain1.wav");
+	PRECACHE_SOUND("barney/ba_pain2.wav");
+	PRECACHE_SOUND("barney/ba_pain3.wav");
+
+	PRECACHE_SOUND("barney/ba_die1.wav");
+	PRECACHE_SOUND("barney/ba_die2.wav");
+	PRECACHE_SOUND("barney/ba_die3.wav");
+
+	// every new barney must call this, otherwise
+	// when a level is loaded, nobody will talk (time is reset to 0)
+	TalkInit();
+	CTalkMonster::Precache();
+}
+
+void CDrillSergeant::BarneyFirePistol()
+{
+	Vector vecShootOrigin;
+
+	UTIL_MakeVectors(pev->angles);
+	vecShootOrigin = pev->origin + Vector(0, 0, 55);
+	Vector vecShootDir = ShootAtEnemy(vecShootOrigin);
+
+	Vector angDir = UTIL_VecToAngles(vecShootDir);
+	SetBlending(0, angDir.x);
+	pev->effects = EF_MUZZLEFLASH;
+
+	FireBullets(1, vecShootOrigin, vecShootDir, VECTOR_CONE_2DEGREES, 1024, BULLET_MONSTER_9MM);
+
+	int pitchShift = RANDOM_LONG(0, 20);
+
+	// Only shift about half the time
+	if (pitchShift > 10)
+		pitchShift = 0;
+	else
+		pitchShift -= 5;
+	EMIT_SOUND_DYN(ENT(pev), CHAN_WEAPON, "barney/ba_attack2.wav", 1, ATTN_NORM, 0, 100 + pitchShift);
+
+	CSoundEnt::InsertSound(bits_SOUND_COMBAT, pev->origin, 384, 0.3);
+
+	// UNDONE: Reload?
+	m_cAmmoLoaded--;// take away a bullet!
+}
+
+void CDrillSergeant::HandleAnimEvent(MonsterEvent_t* pEvent)
+{
+	switch (pEvent->event)
+	{
+	case BARNEY_AE_SHOOT:
+		BarneyFirePistol();
+		break;
+
+	case BARNEY_AE_DRAW:
+		// barney's bodygroup switches here so he can pull gun from holster
+		pev->body = BARNEY_BODY_GUNDRAWN;
+		m_fGunDrawn = TRUE;
+		break;
+
+	case BARNEY_AE_HOLSTER:
+		// change bodygroup to replace gun in holster
+		pev->body = BARNEY_BODY_GUNHOLSTERED;
+		m_fGunDrawn = FALSE;
+		break;
+
+	default:
+		CTalkMonster::HandleAnimEvent(pEvent);
+	}
+}
+
+void CDrillSergeant::TalkInit()
+{
+
+	CTalkMonster::TalkInit();
+
+	// scientists speach group names (group names are in sentences.txt)
+
+	m_szGrp[TLK_ANSWER] = "DR_ANSWER";
+	m_szGrp[TLK_QUESTION] = "DR_QUESTION";
+	m_szGrp[TLK_IDLE] = "DR_IDLE";
+	m_szGrp[TLK_STARE] = "DR_STARE";
+	m_szGrp[TLK_USE] = "DR_OK";
+	m_szGrp[TLK_UNUSE] = "DR_WAIT";
+	m_szGrp[TLK_STOP] = "DR_STOP";
+
+	m_szGrp[TLK_NOSHOOT] = "DR_SCARED";
+	m_szGrp[TLK_HELLO] = "DR_HELLO";
+
+	m_szGrp[TLK_PLHURT1] = "!DR_CUREA";
+	m_szGrp[TLK_PLHURT2] = "!DR_CUREB";
+	m_szGrp[TLK_PLHURT3] = "!DR_CUREC";
+
+	m_szGrp[TLK_PHELLO] = NULL;	//"BA_PHELLO";		// UNDONE
+	m_szGrp[TLK_PIDLE] = NULL;	//"BA_PIDLE";			// UNDONE
+	m_szGrp[TLK_PQUESTION] = "DR_PQUEST";		// UNDONE
+
+	m_szGrp[TLK_SMELL] = "DR_SMELL";
+
+	m_szGrp[TLK_WOUND] = "DR_WOUND";
+	m_szGrp[TLK_MORTAL] = "DR_MORTAL";
+
+	// get voice for head - just one barney voice for now
+	m_voicePitch = 100;
+}
+
+int CDrillSergeant::ObjectCaps()
+{
+	return FCAP_ACROSS_TRANSITION | FCAP_IMPULSE_USE;
+}
