41ad2c6cbb38b82af4f1d88a0dec1a3c7ab8c210
Sam V <sam.vanheer@outlook.com>
2021-12-05 23:42:43 +0100

    Fix Penguin grenade changing movetype unconditionally Resolves ValveSoftware/halflife#3175

--- dlls/penguin_grenade.cpp
+++ dlls/penguin_grenade.cpp
@@ -341,7 +341,7 @@ void CPenguinGrenade::HuntThink()
 		pev->velocity = pev->velocity * 0.9;
 		pev->velocity.z += 8.0;
 	}
-	else if (pev->movetype = MOVETYPE_FLY)
+	else if (pev->movetype == MOVETYPE_FLY)
 	{
 		pev->movetype = MOVETYPE_BOUNCE;
 	}
