ValveSoftware/halflife

Fixed incorrect field types.

dlls/animating.cpp
@@ -28,11 +28,11 @@
 
 TYPEDESCRIPTION	CBaseAnimating::m_SaveData[] = 
 {
-	DEFINE_FIELD( CBaseMonster, m_flFrameRate, FIELD_FLOAT ),
-	DEFINE_FIELD( CBaseMonster, m_flGroundSpeed, FIELD_FLOAT ),
-	DEFINE_FIELD( CBaseMonster, m_flLastEventCheck, FIELD_TIME ),
-	DEFINE_FIELD( CBaseMonster, m_fSequenceFinished, FIELD_BOOLEAN ),
-	DEFINE_FIELD( CBaseMonster, m_fSequenceLoops, FIELD_BOOLEAN ),
+	DEFINE_FIELD( CBaseAnimating, m_flFrameRate, FIELD_FLOAT ),
+	DEFINE_FIELD( CBaseAnimating, m_flGroundSpeed, FIELD_FLOAT ),
+	DEFINE_FIELD( CBaseAnimating, m_flLastEventCheck, FIELD_TIME ),
+	DEFINE_FIELD( CBaseAnimating, m_fSequenceFinished, FIELD_BOOLEAN ),
+	DEFINE_FIELD( CBaseAnimating, m_fSequenceLoops, FIELD_BOOLEAN ),
 };
 
 IMPLEMENT_SAVERESTORE( CBaseAnimating, CBaseDelay );

twhl-community/halflife-unified-sdk

src/game/server/entities/animating.cpp
@@ -29,11 +29,11 @@
 
 TYPEDESCRIPTION	CBaseAnimating::m_SaveData[] =
 {
-	DEFINE_FIELD(CBaseMonster, m_flFrameRate, FIELD_FLOAT),
-	DEFINE_FIELD(CBaseMonster, m_flGroundSpeed, FIELD_FLOAT),
-	DEFINE_FIELD(CBaseMonster, m_flLastEventCheck, FIELD_TIME),
-	DEFINE_FIELD(CBaseMonster, m_fSequenceFinished, FIELD_BOOLEAN),
-	DEFINE_FIELD(CBaseMonster, m_fSequenceLoops, FIELD_BOOLEAN),
+	DEFINE_FIELD(CBaseAnimating, m_flFrameRate, FIELD_FLOAT),
+	DEFINE_FIELD(CBaseAnimating, m_flGroundSpeed, FIELD_FLOAT),
+	DEFINE_FIELD(CBaseAnimating, m_flLastEventCheck, FIELD_TIME),
+	DEFINE_FIELD(CBaseAnimating, m_fSequenceFinished, FIELD_BOOLEAN),
+	DEFINE_FIELD(CBaseAnimating, m_fSequenceLoops, FIELD_BOOLEAN),
 };
 
 IMPLEMENT_SAVERESTORE(CBaseAnimating, CBaseDelay);