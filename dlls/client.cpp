0913709b42845aca308e6e37d013453b29ff7094
Sam V <sam.vanheer@outlook.com>
2022-01-23 19:04:35 +0100

    Fix CTF jump pack item not playing sound when long jumping Resolves #37

--- dlls/client.cpp
+++ dlls/client.cpp
@@ -985,6 +985,8 @@ void ClientPrecache()
 	PRECACHE_SOUND("player/geiger2.wav");
 	PRECACHE_SOUND("player/geiger1.wav");
 
+	PRECACHE_SOUND("ctf/pow_big_jump.wav");
+
 	if (giPrecacheGrunt)
 		UTIL_PrecacheOther("monster_human_grunt");
 }
