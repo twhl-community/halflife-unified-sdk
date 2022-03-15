2ce82ecb2b105dc9194ad798b8b76874ee4bf625
Sam V <sam.vanheer@outlook.com>
2022-03-15 13:30:38 +0100

    Fix Black Ops Apache not firing trigger targets Resolves ValveSoftware/halflife#3262

--- dlls/blkop_apache.cpp
+++ dlls/blkop_apache.cpp
@@ -126,6 +126,7 @@ void COFBlackOpsApache::Spawn()
 	pev->flags |= FL_MONSTER;
 	pev->takedamage = DAMAGE_AIM;
 	pev->health = gSkillData.apacheHealth;
+	pev->max_health = pev->health;
 
 	m_flFieldOfView = -0.707; // 270 degrees
 
@@ -137,14 +138,16 @@ void COFBlackOpsApache::Spawn()
 
 	if ((pev->spawnflags & SF_WAITFORTRIGGER) != 0)
 	{
+		SetThink(&COFBlackOpsApache::NullThink);
 		SetUse(&COFBlackOpsApache::StartupUse);
 	}
 	else
 	{
 		SetThink(&COFBlackOpsApache::HuntThink);
 		SetTouch(&COFBlackOpsApache::FlyTouch);
-		pev->nextthink = gpGlobals->time + 1.0;
 	}
+
+	pev->nextthink = gpGlobals->time + 1.0;
 
 	m_iRockets = 10;
 }
@@ -178,6 +181,7 @@ void COFBlackOpsApache::Precache()
 void COFBlackOpsApache::NullThink()
 {
 	StudioFrameAdvance();
+	FCheckAITrigger();
 	pev->nextthink = gpGlobals->time + 0.5;
 }
 
@@ -205,6 +209,7 @@ void COFBlackOpsApache::Killed(entvars_t* pevAttacker, int iGib)
 	pev->nextthink = gpGlobals->time + 0.1;
 	pev->health = 0;
 	pev->takedamage = DAMAGE_NO;
+	pev->deadflag = DEAD_DYING;
 
 	if ((pev->spawnflags & SF_NOWRECKAGE) != 0)
 	{
@@ -226,6 +231,8 @@ void COFBlackOpsApache::DyingThink()
 	// still falling?
 	if (m_flNextRocket > gpGlobals->time)
 	{
+		FCheckAITrigger();
+
 		// random explosions
 		MESSAGE_BEGIN(MSG_PVS, SVC_TEMPENTITY, pev->origin);
 		WRITE_BYTE(TE_EXPLOSION); // This just makes a dynamic light now
@@ -464,7 +471,19 @@ void COFBlackOpsApache::HuntThink()
 	{
 		Look(4092);
 		m_hEnemy = BestVisibleEnemy();
+
+		//If i have an enemy i'm in combat, otherwise i'm patrolling.
+		if (m_hEnemy != nullptr)
+		{
+			m_MonsterState = MONSTERSTATE_COMBAT;
+		}
+		else
+		{
+			m_MonsterState = MONSTERSTATE_ALERT;
+		}
 	}
+
+	Listen();
 
 	// generic speed up
 	if (m_flGoalSpeed < 800)
@@ -587,6 +606,8 @@ void COFBlackOpsApache::HuntThink()
 			}
 		}
 	}
+
+	FCheckAITrigger();
 }
 
 
@@ -916,7 +937,32 @@ bool COFBlackOpsApache::TakeDamage(entvars_t* pevInflictor, entvars_t* pevAttack
 	*/
 
 	// ALERT( at_console, "%.0f\n", flDamage );
-	return CBaseEntity::TakeDamage(pevInflictor, pevAttacker, flDamage, bitsDamageType);
+	const bool result = CBaseEntity::TakeDamage(pevInflictor, pevAttacker, flDamage, bitsDamageType);
+
+	//Are we damaged at all?
+	if (pev->health < pev->max_health)
+	{
+		//Took some damage.
+		SetConditions(bits_COND_LIGHT_DAMAGE);
+
+		if (pev->health < (pev->max_health / 2))
+		{
+			//Seriously damaged now.
+			SetConditions(bits_COND_HEAVY_DAMAGE);
+		}
+		else
+		{
+			//Maybe somebody healed us somehow (trigger_hurt with negative damage?), clear this.
+			ClearConditions(bits_COND_HEAVY_DAMAGE);
+		}
+	}
+	else
+	{
+		//Maybe somebody healed us somehow (trigger_hurt with negative damage?), clear this.
+		ClearConditions(bits_COND_LIGHT_DAMAGE);
+	}
+
+	return result;
 }
 
 
