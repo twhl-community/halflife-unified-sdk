d2ef1c04a741a1a7409a2fedee41bc009c90c7db
Sam V <sam.vanheer@outlook.com>
2022-06-06 17:26:11 +0200

    Specifiy G++11 in toolchain file, don't try to override compiler in CI config

--- cmake/LinuxToolchain.cmake
+++ cmake/LinuxToolchain.cmake
@@ -3,9 +3,9 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR i386)
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER gcc)
+set(CMAKE_C_COMPILER gcc-11)
 set(CMAKE_C_FLAGS -m32)
-set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_CXX_COMPILER g++-11)
 set(CMAKE_CXX_FLAGS -m32)
 
 # here is the target environment located
