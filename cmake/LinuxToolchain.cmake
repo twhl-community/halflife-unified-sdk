ee80b1216a29b89b38b844d6856650cec3d566cd
SamVanheer <sam.vanheer@outlook.com>
2022-06-18 16:16:46 +0200

    Remove unnecessary Linux toolchain configuration

--- cmake/LinuxToolchain.cmake
+++ cmake/LinuxToolchain.cmake
@@ -8,13 +8,3 @@ set(CMAKE_C_FLAGS -m32)
 set(CMAKE_CXX_COMPILER g++-11)
 set(CMAKE_CXX_FLAGS -m32)
 
-# here is the target environment located
-#set(CMAKE_FIND_ROOT_PATH /usr/lib/i386-linux-gnu)
-
-# adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search
-# programs in the host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
