01eb2864f1758128b78257b25b2c0a144d815f24
Sam V <sam.vanheer@outlook.com>
2022-07-22 23:15:55 +0200

    Add EASTL dependency Resolves  #362

---
+++ src/public/eastl_allocator.cpp
@@ -0,0 +1,32 @@
+/***
+ *
+ *	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+ *
+ *	This product contains software technology licensed from Id
+ *	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+ *	All Rights Reserved.
+ *
+ *   Use, distribution, and modification of this source code and/or resulting
+ *   object code is restricted to non-commercial enhancements to products from
+ *   Valve LLC.  All other use, distribution, or modification is prohibited
+ *   without written permission from Valve LLC.
+ *
+ ****/
+
+#include <new>
+#include <EASTL/allocator.h>
+
+#include "cbase.h"
+
+// See https://github.com/electronicarts/EASTL/blob/master/doc/CMake/EASTL_Project_Integration.md
+
+void* __cdecl operator new[](size_t size, const char* name, int flags, unsigned debugFlags, const char* file, int line)
+{
+	return new uint8_t[size];
+}
+
+void* __cdecl operator new[](size_t size, size_t alignment, size_t alignmentOffset,
+	const char* pName, int flags, unsigned debugFlags, const char* file, int line)
+{
+	return new uint8_t[size];
+}
