81c068510935d19cb51bbce58fea37164ece7fda
Sam V <sam.vanheer@outlook.com>
2023-03-25 15:23:09 +0100

    Implement player_sethealth

--- src/game/server/entities/player_entities.cpp
+++ src/game/server/entities/player_entities.cpp
@@ -185,7 +185,7 @@ bool CStripWeapons::KeyValue(KeyValueData* pkvd)
 
 void CStripWeapons::Use(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value)
 {
-	const auto executor = [&](CBasePlayer* player)
+	const auto executor = [this](CBasePlayer* player)
 	{
 		if ((m_Flags & StripFlagRemoveWeapons) != 0)
 		{
@@ -215,6 +215,136 @@ void CStripWeapons::Use(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE 
 		CBasePlayer* player = ToBasePlayer(pActivator);
 
 		if (!player && !g_pGameRules->IsDeathmatch())
+		{
+			player = UTIL_GetLocalPlayer();
+		}
+
+		if (player)
+		{
+			executor(player);
+		}
+	}
+}
+
+/**
+ *	@brief Sets the player's health and/or armor to a mapper-specified value
+ */
+class CPlayerSetHealth : public CPointEntity
+{
+public:
+	static constexpr int FlagAllPlayers = 1 << 0;
+	static constexpr int FlagSetHealth = 1 << 1;
+	static constexpr int FlagSetArmor = 1 << 2;
+
+	bool Save(CSave& save) override;
+	bool Restore(CRestore& restore) override;
+	static TYPEDESCRIPTION m_SaveData[];
+
+	bool KeyValue(KeyValueData* pkvd) override;
+
+	void Use(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value) override;
+
+private:
+	int m_Flags = 0;
+	int m_Health = 100;
+	int m_Armor = 0;
+};
+
+LINK_ENTITY_TO_CLASS(player_sethealth, CPlayerSetHealth);
+
+TYPEDESCRIPTION CPlayerSetHealth::m_SaveData[] =
+	{
+		DEFINE_FIELD(CPlayerSetHealth, m_Flags, FIELD_INTEGER),
+		DEFINE_FIELD(CPlayerSetHealth, m_Health, FIELD_INTEGER),
+		DEFINE_FIELD(CPlayerSetHealth, m_Armor, FIELD_INTEGER)};
+
+IMPLEMENT_SAVERESTORE(CPlayerSetHealth, CPointEntity);
+
+bool CPlayerSetHealth::KeyValue(KeyValueData* pkvd)
+{
+	if (FStrEq(pkvd->szKeyName, "all_players"))
+	{
+		if (atoi(pkvd->szValue) != 0)
+		{
+			SetBits(m_Flags, FlagAllPlayers);
+		}
+		else
+		{
+			ClearBits(m_Flags, FlagAllPlayers);
+		}
+
+		return true;
+	}
+	else if (FStrEq(pkvd->szKeyName, "set_health"))
+	{
+		if (atoi(pkvd->szValue) != 0)
+		{
+			SetBits(m_Flags, FlagSetHealth);
+		}
+		else
+		{
+			ClearBits(m_Flags, FlagSetHealth);
+		}
+
+		return true;
+	}
+	else if (FStrEq(pkvd->szKeyName, "set_armor"))
+	{
+		if (atoi(pkvd->szValue) != 0)
+		{
+			SetBits(m_Flags, FlagSetArmor);
+		}
+		else
+		{
+			ClearBits(m_Flags, FlagSetArmor);
+		}
+
+		return true;
+	}
+	else if (FStrEq(pkvd->szKeyName, "health_to_set"))
+	{
+		// Clamp to 1 so players don't end up in an invalid state
+		m_Health = std::max(atoi(pkvd->szValue), 1);
+		return true;
+	}
+	else if (FStrEq(pkvd->szKeyName, "armor_to_set"))
+	{
+		m_Armor = std::max(atoi(pkvd->szValue), 0);
+		return true;
+	}
+
+	return CPointEntity::KeyValue(pkvd);
+}
+
+void CPlayerSetHealth::Use(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value)
+{
+	const auto executor = [this](CBasePlayer* player)
+	{
+		if ((m_Flags & FlagSetHealth) != 0)
+		{
+			// Clamp it to the current max health
+			player->pev->health = std::min(static_cast<int>(std::floor(player->pev->max_health)), m_Health);
+		}
+
+		if ((m_Flags & FlagSetArmor) != 0)
+		{
+			// Clamp it now, in case future changes allow for custom armor maximum
+			player->pev->armorvalue = std::min(MAX_NORMAL_BATTERY, m_Armor);
+		}
+	};
+
+	if ((m_Flags & FlagAllPlayers) != 0)
+	{
+		for (auto player : UTIL_FindPlayers())
+		{
+			executor(player);
+		}
+	}
+	else
+	{
+		CBasePlayer* player = ToBasePlayer(pActivator);
+
+		if (!player && !g_pGameRules->IsMultiplayer())
 		{
 			player = UTIL_GetLocalPlayer();
 		}
